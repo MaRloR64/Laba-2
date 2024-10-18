22eacf71cf4a9954da3a62a3a4551d69939192ad
MaRloR64 <eyk230679@gmail.com>
2024-10-18 18:09:01 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int X, Y, Z;
+
+    // Ввод чисел X, Y, Z с клавиатуры
+    cout << "Введите число X: ";
+    cin >> X;
+    cout << "Введите число Y: ";
+    cin >> Y;
+    cout << "Введите число Z: ";
+    cin >> Z;
+
+    // Проверка первого условия: каждое из чисел X и Y нечетное
+    bool condition1 = (X % 2 != 0) && (Y % 2 != 0);
+
+    // Проверка второго условия: только одно из чисел X и Y меньше 20
+    bool condition2 = ((X < 20) && (Y >= 20)) || ((X >= 20) && (Y < 20));
+
+    // Проверка третьего условия: хотя бы одно из чисел X и Y равно нулю
+    bool condition3 = (X == 0) || (Y == 0);
+
+    // Проверка четвертого условия: каждое из чисел X, Y, Z отрицательное
+    bool condition4 = (X < 0) && (Y < 0) && (Z < 0);
+
+    // Проверка пятого условия: только одно из чисел X, Y и Z кратно пяти
+    bool condition5 = ((X % 5 == 0) && (Y % 5 != 0) && (Z % 5 != 0)) ||
+        ((X % 5 != 0) && (Y % 5 == 0) && (Z % 5 != 0)) ||
+        ((X % 5 != 0) && (Y % 5 != 0) && (Z % 5 == 0));
+
+    // Проверка шестого условия: хотя бы одно из чисел X, Y, Z больше 100
+    bool condition6 = (X > 100) || (Y > 100) || (Z > 100);
+
+    // Общая проверка всех условий
+    bool finalCondition = condition1 && condition2 && condition3 && condition4 && condition5 && condition6;
+
+    // Вывод результата
+    if (finalCondition) {
+        cout << "condition is true" << endl;
+    }
+    else {
+        cout << "condition is false" << endl;
+    }
+
+    return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+// Функция для вычисления произведения чисел в заданном диапазоне
+long long product(int start, int end) {
+    long long result = 1;
+    for (int i = start; i <= end; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    // Произведение всех целых чисел от 8 до 15
+    std::cout << "Произведение всех целых чисел от 8 до 15: " << product(8, 15) << std::endl;
+
+    // Ввод числа a с клавиатуры
+    int a;
+    std::cout << "Введите число a (1 < a < 20): ";
+    std::cin >> a;
+
+    // Произведение всех целых чисел от a до 20
+    std::cout << "Произведение всех целых чисел от " << a << " до 20: " << product(a, 20) << std::endl;
+
+    // Ввод числа b с клавиатуры
+    int b;
+    std::cout << "Введите число b (1 < b < 20): ";
+    std::cin >> b;
+
+    // Произведение всех целых чисел от 1 до b
+    std::cout << "Произведение всех целых чисел от 1 до " << b << ": " << product(1, b) << std::endl;
+
+    // Ввод чисел a и b с клавиатуры
+    std::cout << "Введите число a: ";
+    std::cin >> a;
+    std::cout << "Введите число b (b >= a): ";
+    std::cin >> b;
+
+    // Произведение всех целых чисел от a до b
+    std::cout << "Произведение всех целых чисел от " << a << " до " << b << ": " << product(a, b) << std::endl;
+
+    return 0;
+}
